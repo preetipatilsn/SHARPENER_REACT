85685372f0d9049a17417be85ca92c5450639e9c
preetipatilsn <preetipatilsn4@gmail.com>
2022-12-03 22:00:08 +0530

    task 10 done

---
+++ react_learning/src/components/ExpenseDetails.js
@@ -0,0 +1,10 @@
+// import './ExpenseItem.css';
+
+// function ExpenseDetails(props){
+//    return (<div><div><h2 className='expense-item__title'>{props.title}</h2></div>
+    
+//     <div className='expense-item__price'>{props.amount}</div></div>
+//     );
+// };
+
+// export default ExpenseDetails;
--- react_learning/src/components/ExpenseItem.js
+++ react_learning/src/components/ExpenseItem.js
@@ -1,14 +1,18 @@
+import ExpenseDate from './ExpenseDate';
+// import ExpenseDetails from './ExpenseDetails';
 import './ExpenseItem.css';
 
 function ExpenseItem(props){
+
     return (
     <div className='expense-item'>
-    <div>{props.date.toISOString()}</div>
+        <ExpenseDate date={props.date} />
     <div className='expense-item__description'>
-        <h2>{props.title}</h2>
-        <div className='expense-item__location'>{props.location}</div>
-        <div className='expense-item__price'>Rs{props.amount}</div>
+    <h2 className='expense-item__title'>{props.title}</h2>
+    <div className='expense-item__location'>{props.location}</div>
+    <div className='expense-item__price'>{props.amount}</div></div>
+    
     </div>
-    </div>)
+    )
 }
 export default ExpenseItem;
