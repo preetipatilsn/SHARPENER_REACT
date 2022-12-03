e015bfa2baef30041758d558dea3c469461633fb
preetipatilsn <preetipatilsn4@gmail.com>
2022-12-03 21:06:28 +0530

    task 9 adding 4 expenseitems

--- react_learning/src/components/ExpenseItem.js
+++ react_learning/src/components/ExpenseItem.js
@@ -1,5 +1,14 @@
-function ExpenseItem(){
-    return <div><div>january 7 2001</div>
-    <div><h2>Car Insurance</h2><div>Rs.4774</div></div></div>
+import './ExpenseItem.css';
+
+function ExpenseItem(props){
+    return (
+    <div className='expense-item'>
+    <div>{props.date.toISOString()}</div>
+    <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__location'>{props.location}</div>
+        <div className='expense-item__price'>Rs{props.amount}</div>
+    </div>
+    </div>)
 }
-export default ExpenseItem; 
+export default ExpenseItem;
