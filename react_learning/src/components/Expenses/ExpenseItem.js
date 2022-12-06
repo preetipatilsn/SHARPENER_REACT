299a9a3c65d361a59f032c66f7c6faefd9d49ded
preetipatilsn <preetipatilsn4@gmail.com>
2022-12-06 21:43:37 +0530

    added edit title and delete title

--- react_learning/src/components/Expenses/ExpenseItem.js
+++ react_learning/src/components/Expenses/ExpenseItem.js
@@ -2,14 +2,26 @@ import React from 'react';
 
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
+// import Card from '../UI/card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
 
+    const clickHandler = () =>{
+        console.log("clicked!");
+    }
+
+    const deleteHandler = () =>{
+        console.log("clicked!");
+    }
+
     return (
     <div className='expense-item'>
         <ExpenseDate date={props.date} />
-        <ExpenseDetails title={props.title} amount={props.amount} location={props.location} />
+        <ExpenseDetails title={props.title} amount={props.amount} />
+        <button onClick={clickHandler}>Change Title</button>
+        <button onClick={deleteHandler}>Delete Title</button>
+
     </div>)
 };
 export default ExpenseItem;
