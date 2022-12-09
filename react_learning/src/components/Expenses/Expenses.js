2f54d5d9e1e5c4e0f35bda698b1a009ac2caae34
preetipatilsn <preetipatilsn4@gmail.com>
2022-12-10 03:51:50 +0530

    task 11 filter by year

--- react_learning/src/components/Expenses/Expenses.js
+++ react_learning/src/components/Expenses/Expenses.js
@@ -8,15 +8,18 @@ const Expenses = (props) => {
 
     const [filteredYear, setFilteredYear] = useState('2020');
 
-
     const filterChangeHandler = selectedYear => {
-    setFilteredYear(selectedYear);
-    }
+        setFilteredYear(selectedYear);
+    };
+
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
     
     return(
         <div className='expenses'>
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        {props.items.map(expense => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />)}
+        {filteredExpenses.map(expense => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />)}
         </div>
     );
 };
